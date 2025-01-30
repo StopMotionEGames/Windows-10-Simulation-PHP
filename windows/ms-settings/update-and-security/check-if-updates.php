881799a4f84b50aa5e6154dce8ac59344c4f8642
Stop Motion & Games - Dev <ravicwidnef@gmail.com>
2025-01-30 12:25:26 -0300

    Atualiza o caminho do script de verificação de atualizações e melhora o tratamento da saída, adicionando logs de depuração.

--- windows/ms-settings/update-and-security/check-if-updates.php
+++ windows/ms-settings/update-and-security/check-if-updates.php
@@ -1,38 +1,28 @@
 <?php
-putenv('GIT_TERMINAL_PROMPT=1');
-putenv('GIT_ASKPASS=/usr/bin/echo');
-putenv('GIT_SSH_COMMAND=ssh -o "StrictHostKeyChecking=no"');
 // Caminho para o script shell
-$scriptPath = '/var/www/html/check-for-updates.sh';
+$scriptPath = '/var/www/html/check-updates.sh';
 
-$descriptorspec = array(
-    0 => array("pipe", "r"),  // stdin
-    1 => array("pipe", "w"),  // stdout
-    2 => array("pipe", "w")   // stderr
-);
+// Definir variável de ambiente HOME
+putenv('HOME=/var/www');
 
-$process = proc_open('bash ' . $scriptPath, $descriptorspec, $pipes, null, null);
+// Adicionar diretório seguro antes de executar o script
+shell_exec("git config --global --add safe.directory /var/www/html");
 
-if (is_resource($process)) {
-    $output = stream_get_contents($pipes[1]);
-    fclose($pipes[1]);
+// Executar o script shell
+$output = shell_exec("bash $scriptPath 2>&1");
 
-    $error_output = stream_get_contents($pipes[2]);
-    fclose($pipes[2]);
+// Processar a saída do script shell
+$output_lines = explode("\n", trim($output));
+$status = array_shift($output_lines); // Pega a primeira linha como status
 
-    $return_value = proc_close($process);
+// Adicionar log de depuração
+file_put_contents('/home/smeg/docker-compose/html/check-updates-php.log', "Output:\n$output\n", FILE_APPEND);
 
-    // Adicionar log de depuração
-    file_put_contents('/var/www/html/check-updates.log', "Output:\n$output\nError Output:\n$error_output\nReturn Value:\n$return_value\n", FILE_APPEND);
-
-    $output_lines = explode("\n", trim($output));
-    $status = array_shift($output_lines); // Pega a primeira linha como status
-
-    if ($status === "update") {
-        echo json_encode(["message" => "Algumas atualizações foram encontradas.", "status" => "update", "files" => $output_lines]);
-    } elseif ($status === "no_update") {
-        echo json_encode(["message" => "Nenhuma atualização encontrada.", "status" => "no_update"]);
-    } else {
-        echo json_encode(["message" => "Erro: $status", "status" => "error"]);
-    }
+if ($status === "update") {
+    echo json_encode(["message" => "Algumas atualizações foram encontradas.", "status" => "update", "files" => $output_lines]);
+} elseif ($status === "no_update") {
+    echo json_encode(["message" => "Nenhuma atualização encontrada.", "status" => "no_update"]);
+} else {
+    echo json_encode(["message" => "Erro: $status", "status" => "error"]);
 }
+?>
