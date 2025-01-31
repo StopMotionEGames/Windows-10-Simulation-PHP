8be86f746385d46a86467d3a00581d65e954dbc4
Stop Motion & Games - Dev <ravicwidnef@gmail.com>
2025-01-31 02:10:03 -0300

    use getEnv

--- functions/download_update.php
+++ functions/download_update.php
@@ -9,82 +9,105 @@ error_reporting(E_ALL);
 // Definir cabeçalho para JSON
 header('Content-Type: application/json');
 
-function getCurrentVersion($root)
+$debug = true;
+$logs = '';
+
+function getCurrentVersion($root, $debug, &$logs)
 {
   $jsonFilePath = "$root/Windows/System32/winver.json";
   if (file_exists($jsonFilePath)) {
     $jsonData = file_get_contents($jsonFilePath);
+    if ($debug)
+      $logs .= "Current version data: " . $jsonData . "\n";
     return json_decode($jsonData, true);
   } else {
+    if ($debug)
+      $logs .= "Current version file not found.\n";
     return null;
   }
 }
 
-function checkForUpdates($currentVersion)
+function checkForUpdates($currentVersion, $debug, &$logs)
 {
-  $githubApiUrl = 'https://api.github.com/repos/StopMotionEGames/Windows-10-Simulation-PHP/releases/latest';
-  $token = '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';
-  $logs = '';
+  $githubApiUrl = 'https://api.github.com/repos/StopMotionEGames/Windows-10-Simulation-PHP/releases';
+  $token = getenv("GITHUB_TOKEN");
 
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $githubApiUrl);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_USERAGENT, 'PHP');
   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Authorization: token ' . $token
+    'Authorization: Bearer ' . $token,
+    'Accept: application/vnd.github+json',
+    'X-GitHub-Api-Version: 2.23.28'
   ));
   $response = curl_exec($ch);
   $curlError = curl_error($ch);
+  $curlInfo = curl_getinfo($ch);
   curl_close($ch);
+
+  if ($debug) {
+    $logs .= "cURL Info:\n" . print_r($curlInfo, true) . "\n";
+    $logs .= "cURL Error:\n" . $curlError . "\n";
+    $logs .= "API Response:\n" . $response . "\n";
+  }
 
   if (!$response) {
     $logs .= "Falha ao obter resposta da API do GitHub. Erro: $curlError\n";
     return ['logs' => $logs];
   }
 
-  $releaseData = json_decode($response, true);
+  $releasesData = json_decode($response, true);
   if (json_last_error() !== JSON_ERROR_NONE) {
     $logs .= "Erro ao decodificar JSON da resposta do GitHub: " . json_last_error_msg() . "\n";
     return ['logs' => $logs];
   }
 
-  if (!isset($releaseData['tag_name']) || !isset($releaseData['body'])) {
-    $logs .= "Dados de release inválidos obtidos da API do GitHub.\n";
+  // Filtrar pelo release mais recente para o branch específico
+  $latestRelease = null;
+  foreach ($releasesData as $release) {
+    if (strpos($release['tag_name'], $currentVersion['branch']) !== false) {
+      $latestRelease = $release;
+      break;
+    }
+  }
+
+  if (!$latestRelease) {
+    $logs .= "Nenhum release encontrado para o branch: " . $currentVersion['branch'] . "\n";
     return ['logs' => $logs];
   }
 
   // Extrair as três primeiras linhas da descrição
-  $descriptionLines = explode(PHP_EOL, $releaseData['body']);
+  $descriptionLines = explode(PHP_EOL, $latestRelease['body']);
   $version = isset($descriptionLines[0]) ? trim($descriptionLines[0]) : '';
   $compilation = isset($descriptionLines[1]) ? trim($descriptionLines[1]) : '';
   $update = isset($descriptionLines[2]) ? trim($descriptionLines[2]) : '';
 
-  // Verificar se a nova versão é maior que 0.0.0 e se o branch é o correto
-  if ($releaseData['tag_name'] > 'v0.0.0' && strpos($releaseData['tag_name'], $currentVersion['branch']) !== false) {
-    $logs .= "Nova atualização encontrada: {$releaseData['tag_name']}\n";
+  if ($latestRelease['tag_name'] > $currentVersion['tag']) {
+    $logs .= "Nova atualização encontrada: {$latestRelease['tag_name']}\n";
     $logs .= "Versão: $version\n";
     $logs .= "Compilação: $compilation\n";
     $logs .= "Update: $update\n";
     return [
-      'releaseData' => $releaseData,
+      'releaseData' => $latestRelease,
       'version' => $version,
       'compilation' => $compilation,
       'update' => $update,
       'logs' => $logs
     ];
   } else {
-    $logs .= "Nenhuma atualização disponível ou branch incompatível.\n";
+    $logs .= "Nenhuma atualização disponível ou release incompatível.\n";
     return ['logs' => $logs];
   }
 }
 
-function downloadAndUpdate($releaseData, $root)
+function downloadAndUpdate($releaseData, $root, $debug, &$logs)
 {
   $tempDir = "$root/Windows/SoftwareDistribution/Temp";
   $logs = '';
 
   $downloadUrl = '';
-  foreach ($releaseData['releaseData']['assets'] as $asset) {
+  foreach ($releaseData['assets'] as $asset) {
     if (strpos($asset['name'], 'update') !== false) {
       $downloadUrl = $asset['browser_download_url'];
       break;
@@ -130,10 +153,10 @@ function recursiveMove($src, $dst)
 }
 
 $root = rootDirectory();
-$currentVersion = getCurrentVersion($root);
-$releaseInfo = checkForUpdates($currentVersion);
+$currentVersion = getCurrentVersion($root, $debug, $logs);
+$releaseInfo = checkForUpdates($currentVersion, $debug, $logs);
 if (isset($releaseInfo['releaseData'])) {
-  $logs = downloadAndUpdate($releaseInfo, $root);
+  $logs = downloadAndUpdate($releaseInfo['releaseData'], $root, $debug, $logs);
   echo json_encode([
     'message' => 'Atualização concluída com sucesso!',
     'logs' => $logs
